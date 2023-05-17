1cdda31adffad7e14f01c5570d24c25954dbee23
Jeffrey Zhang <jzhang22la@gmail.com>
2023-05-16 19:36:55 -0700

    minimum requirements met, spinner appears while loading at bottom of page and scrolling to bottom of list does nothing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,6 @@
 	import Loader from 'components/Loader.svelte';
 	import User from 'components/User.svelte';
 	import type { UserType } from 'lib/types';
-	import Layout from './+layout.svelte';
-	import { Result } from 'postcss';
 
 	const client = createClient({
 		url: '/graphql',
@@ -40,38 +38,63 @@
 		// when result is updated, update userlist with the list of new users
 		result.subscribe(({ data }) => {
 			if (data) {
-				userList = userList.concat(data.users);
-				console.log(userList);
+				userList = [...userList, ...data.users];
 			}
 		});
 
 		// returns users for current call
 		return result;
 	};
-
 	// $result.data.users - array of objects (users)
+
 	// initial call of query
 	let result = getUsers();
 
+	// function to load next 10
 	const nextPage = () => {
 		// limit += 10; // adds 10 to the query, but then query has to search from 0 to limit again
 		// updates only 10 users at a time
 		from += limit;
 		result = getUsers();
 	};
+
+	// using bind for svelte with on:scroll on div
+	let outerDiv: any;
+
+	// update on scroll instead of using button using scroll event listener
+	const handleScroll = () => {
+		const { scrollTop, scrollHeight, clientHeight } = outerDiv;
+		// console.log('positions', scrollTop, scrollHeight, clientHeight);
+		// console.log(scrollTop + clientHeight, scrollHeight);
+
+		// when we reach bottom of page, call load
+		if (scrollTop + clientHeight >= scrollHeight) {
+			nextPage();
+		}
+
+		/* remove handleScroll if no values
+		sends few extra requests because when user scrolls too quickly, "from" might update before
+		we add 10 new users to userList
+		*/
+		if (from > userList.length + 10) {
+			outerDiv.removeEventListener('scroll', handleScroll);
+		} else {
+			outerDiv.addEventListener('scroll', handleScroll);
+		}
+	};
 </script>
 
-<div class="w-full h-full overflow-scroll">
+<div class="w-full h-full overflow-scroll" on:scroll={handleScroll} bind:this={outerDiv}>
 	<div class="flex flex-col gap-4 items-center p-4">
 		<!-- Gets users and displays them -->
 		{#each userList as user (user.id)}
 			<User {user} />
 		{/each}
+		<!-- While loading, display spinner -->
 		{#if $result.fetching}
-			<!-- Spinner that appears on load (while fetching data) -->
 			<Loader />
-		{:else if $result.data && from < userList.length}
-			<button on:click={nextPage}>test load {from}</button>
 		{/if}
+		<!-- Buffer space/ allows us to see we've reached end -->
+		<div style="height:50px" />
 	</div>
 </div>
